2047dee401e3c9ea11772df34e3e68a8d0e48e3d
ChioukhLyes <lchioukh@excilys.com>
2015-04-17 16:48:51 +0200

    feat(model):Creating model User/Updating script db.

---
+++ computer-database-core/src/main/java/com/excilys/models/User.java
@@ -0,0 +1,201 @@
+package com.excilys.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class User.
+ */
+@Entity
+@Table(name = "users")
+public class User {
+
+	/** The id. */
+	@Id
+	private Long id;
+
+	/** The username. */
+	@Column(name = "username")
+	private String username;
+
+	/** The password. */
+	@Column(name = "password")
+	private String password;
+
+	/** The role. */
+	@Column(name = "role")
+	private Long role;
+
+	/**
+	 * Instantiates a new user.
+	 *
+	 * @param id
+	 *            the id
+	 * @param username
+	 *            the username
+	 * @param password
+	 *            the password
+	 * @param role
+	 *            the role
+	 */
+	public User(Long id, String username, String password, Long role) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.role = role;
+	}
+
+	/**
+	 * Instantiates a new user.
+	 */
+	public User() {
+		super();
+	}
+
+	/**
+	 * Gets the id.
+	 *
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * Sets the id.
+	 *
+	 * @param id
+	 *            the new id
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * Gets the username.
+	 *
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * Sets the username.
+	 *
+	 * @param username
+	 *            the new username
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * Gets the password.
+	 *
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * Sets the password.
+	 *
+	 * @param password
+	 *            the new password
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * Gets the role.
+	 *
+	 * @return the role
+	 */
+	public Long getRole() {
+		return role;
+	}
+
+	/**
+	 * Sets the role.
+	 *
+	 * @param role
+	 *            the new role
+	 */
+	public void setRole(Long role) {
+		this.role = role;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password="
+				+ password + ", role=" + role + "]";
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		result = prime * result
+				+ ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+}
--- computer-database-persistence/src/main/resources/usersRoles.sql
+++ computer-database-persistence/src/main/resources/usersRoles.sql
@@ -9,10 +9,11 @@ create table if not exists users (
   id bigint unsigned not null auto_increment,
   username varchar(100) not null,
   password binary(60) not null,
+  role bigint unsigned not null,
   constraint pk_users primary key(id),
   unique(username)
 );
-
+/*
 create table if not exists roles (
   id bigint unsigned not null auto_increment,
   role varchar(100) not null,
